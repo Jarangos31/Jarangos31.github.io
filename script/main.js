527c0a365ae29cfeb1348b0c2ff8632892a5a75b
Jarangos <jairarango10@gmail.com>
2022-12-10 06:58:14 -0500

    Se agrega boton de dark/white theme.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,12 @@
-function darkMode(){
-  document.body.style.backgroundColor = "#000";
-  document.body.style.color = "#fff";
-  document.getElementById("narrativa").style.color = "#fff";
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+function cambiaTema(ev) {
+  if (ev.target.checked) {
+    document.documentElement.setAttribute("tema", "dark");
+  } else {
+    document.documentElement.setAttribute("tema", "light");
+  }
 }
-
+colorSwitch.addEventListener("change", cambiaTema);
 
 function mostrarResulado() {
   document.getElementById("resultado").style.display = "block";
@@ -16,41 +19,49 @@ function sumar() {
   num1 = document.getElementById("num1").value;
   num2 = document.getElementById("num2").value;
   resultado = parseInt(num1) + parseInt(num2);
-  document.getElementById("Respuesta").innerHTML = `El resultado de sumar ${num1} + ${num2} es:`;
-  document.getElementById("result").value=resultado;
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de sumar ${num1} + ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
 
 function restar() {
-    let num1;
-    let num2;
-    let resultado;
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    resultado = parseInt(num1) - parseInt(num2);
-    document.getElementById("Respuesta").innerHTML = `El resultado de restar ${num1} - ${num2} es:`;
-    document.getElementById("result").value=resultado;
+  let num1;
+  let num2;
+  let resultado;
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  resultado = parseInt(num1) - parseInt(num2);
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de restar ${num1} - ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
 
 function multiplicar() {
-    let num1;
-    let num2;
-    let resultado;
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    resultado = parseInt(num1) * parseInt(num2);
-    document.getElementById("Respuesta").innerHTML = `El resultado de muliplicar ${num1} por ${num2} es:`;
-    document.getElementById("result").value=resultado;
+  let num1;
+  let num2;
+  let resultado;
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  resultado = parseInt(num1) * parseInt(num2);
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de muliplicar ${num1} por ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
 
 function divivir() {
-    let num1;
-    let num2;
-    let resultado;
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    resultado = parseInt(num1) / parseInt(num2);
-    document.getElementById("Respuesta").innerHTML = `El resultado de dividir ${num1} entre ${num2} es:`;
-    document.getElementById("result").value=resultado;
+  let num1;
+  let num2;
+  let resultado;
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  resultado = parseInt(num1) / parseInt(num2);
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de dividir ${num1} entre ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
 function modular() {
   let num1;
@@ -59,8 +70,10 @@ function modular() {
   num1 = document.getElementById("num1").value;
   num2 = document.getElementById("num2").value;
   resultado = parseInt(num1) % parseInt(num2);
-  document.getElementById("Respuesta").innerHTML = `El resultado de modular ${num1} entre ${num2} es:`;
-  document.getElementById("result").value=resultado;
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de modular ${num1} entre ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
 
 function potenciar() {
@@ -69,7 +82,9 @@ function potenciar() {
   let resultado;
   num1 = document.getElementById("num1").value;
   num2 = document.getElementById("num2").value;
-  resultado = Math.pow(parseInt(num1),parseInt(num2))
-  document.getElementById("Respuesta").innerHTML = `El resultado de potenciar ${num1} a ${num2} es:`;
-  document.getElementById("result").value=resultado;
+  resultado = Math.pow(parseInt(num1), parseInt(num2));
+  document.getElementById(
+    "Respuesta"
+  ).innerHTML = `El resultado de potenciar ${num1} a ${num2} es:`;
+  document.getElementById("result").value = resultado;
 }
