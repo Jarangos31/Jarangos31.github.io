9fab1b106bae64e50f74de268d7d93f4b610cea1
Jarangos <jairarango10@gmail.com>
2022-12-15 20:22:53 -0500

    Se corrige error del dark theme.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function cambiaTema(ev) {
   if (ev.target.checked) {
     document.documentElement.setAttribute("tema", "dark");
